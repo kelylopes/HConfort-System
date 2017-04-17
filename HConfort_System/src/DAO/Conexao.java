ca890ed25a6eb939b2bf2a7d08bb00aeeec5baf2
HConfort-System <HConfort-System@20.20.0.11>
2017-04-17 11:32:39 -0300

    Criação do projeto

--- HConfort_System/src/DAO/Conexao.java
+++ HConfort_System/src/DAO/Conexao.java
@@ -3,31 +3,56 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import javax.swing.JOptionPane;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 
 
 public class Conexao {
     
-    public static Connection AbrirConexao(){
-        Connection Connection = null;
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql//localhost:3333/dbhconfortsystem";
+    private static final String USER = "root";
+    private static final String PASE = "admin";
+    
+    
+    public static Connection getConnection(){
+        
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "";
-            Connection = DriverManager.getConnection(url,"admin", "");
-                   
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "Erro em conectar ao Banco de Dados","Funcionario", JOptionPane.ERROR_MESSAGE );
+            Class.forName(DRIVER);
+            
+            return DriverManager.getConnection(URL, URL, DRIVER);
+            
+         
+        } catch (ClassNotFoundException | SQLException ex) {
+           throw new RuntimeException("Erro na conexão com o Banco de Dados", ex);
         }
-        return Connection;
     }
     
-    public static void FecharConexao(Connection Connection){
-        try {
-             Connection.close();
-        } catch (Exception e) {
-            System.out.println(e.getLocalizedMessage());
+    public void closeConnection( Connection con){
+        
+        if (con != null){
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
         
+        
+         public void closeConnection (Connection con, PreparedStatement stmt){
+        
+        if (con != null){
+            try {
+                con.close();
+            } catch (SQLException ex) {
+                Logger.getLogger(Conexao.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
+    
     
 }

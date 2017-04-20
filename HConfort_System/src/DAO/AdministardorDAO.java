efb0fe72d47577c139dd509b9aebe4f6d7d6c2a2
HConfort-System <HConfort-System@20.20.0.11>
2017-04-20 15:36:01 -0300

    Criação do projeto

--- HConfort_System/src/DAO/AdministardorDAO.java
+++ HConfort_System/src/DAO/AdministardorDAO.java
@@ -1,23 +1,14 @@
 
 package DAO;
 
-import hconfort_system.modelo.Administrador;
-import java.sql.Connection;
 
 
 
 
-public class AdministardorDAO extends ExecutarSql{
+public class AdministardorDAO {
 
-    public AdministardorDAO(Connection con) {
-        super(con);
-    }
    
-       public String Inserir_A (Administrador a){
-           String sql = "INSERT INTO administardor VALUES (0,?,?,?)";
-        return null;
-           
    }
     
 
- }
+ 

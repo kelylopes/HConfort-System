10cc9af87c0e2c03846a68f7e8c183beaf72ac0f
Ana Kely Lopes <Ana Kely Lopes@20.20.0.137>
2017-04-26 16:20:53 -0300

    Criaçâo das telas @Ueslei

---
+++ HConfort_System/src/view/Telas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+/**
+ *
+ * @author Ana Kely Lopes
+ */
+public class Telas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TelaInicial telaInicial = new TelaInicial();
+        telaInicial.setVisible(true);
+        
+    }
+    
+}

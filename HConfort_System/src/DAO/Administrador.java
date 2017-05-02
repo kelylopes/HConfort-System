408bbebeab63a18c7b15d5dc28f98575171afc20
HConfort-System <HConfort-System@20.20.0.11>
2017-05-02 10:43:01 -0300

    Cronograma

---
+++ HConfort_System/src/DAO/Administrador.java
@@ -0,0 +1,76 @@
+
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Administrador extends ExecutarSql{
+
+    public Administrador(Connection con) {
+        super(con);
+    }
+    
+      public String Inserir_Administrador(Administrador a){
+        String sql = "INSERT INTO funcionario VALUES (0,?,?,?)";
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            
+            ps.setString(1, a.getLogin());
+            ps.setString(1, a.getSenha());
+            
+            if(ps.executeUpdate() > 0){
+                return " Cadastro com Sucesso!";
+            }else{
+                return "Erro ao cadastrastar o Administrador!";
+            }
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    private String getLogin() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+
+    private String getSenha() {
+        throw new UnsupportedOperationException("Not supported yet."); 
+    }
+    
+    
+    public List<Administrador> Listar_Funcionario(){
+        String sql = "SELECT codigo,nome,login,senha FROM ";
+        List<Administrador> lista = new ArrayList<Administrador>();
+        
+        try {
+            PreparedStatement ps = getCon().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            
+            if(rs != null){
+                while(rs.next()){
+                    Funcionario f = new Funcionario();
+                    f.setCod(rs.getInt(1));
+                    f.setNome(rs.getString(2));
+                    f.setLogin(rs.getString(3));
+                    f.setSenha(rs.getString(4));
+                    
+                    lista.add(f);
+                }
+            return lista;
+            }else{
+                return null;
+            }
+        } catch (Exception e) {
+            return null;
+        }
+        
+    }
+    
+    
+    
+}
+
+
+

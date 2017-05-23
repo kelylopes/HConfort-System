9150d0e640ad8773c75cadb4ec00641549df7eb6
Ana Kely Lopes <Ana Kely Lopes@20.20.1.195>
2017-05-23 19:13:26 -0300

    projeto

--- HConfort_System/src/principal/Main.java
+++ HConfort_System/src/principal/Main.java
@@ -1,30 +1,15 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package principal;
 
 import principal.Login;
 
-/**
- *
- * @author CLAUDIO
- */
-public class Main {
-    
-    public static void main(String[] args) {
-        new Login().setVisible(true);
-=======
-
-package principal;
-
 
 public class Main {
     
-    public static void main(String[] args) {
-        new Menu().setVisible(true);
->>>>>>> origin/master
+    public static void main(String[] args){
+     new Login().setVisible(true);
+   
     }
-}
+    
+    
+       
